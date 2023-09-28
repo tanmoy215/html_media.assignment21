8dc7db428e1020e67609450687b8bec138664257
Tanmoy Majee <139630081+tanmoy215@users.noreply.github.com>
2023-09-28 12:53:18 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float pi = 3.14;
+    float radious =10;
+    float area = 2*pi*radious;
+    cout<<area;
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=2,y=5;
+    cout<<x*y;
+    return 0;
+}
